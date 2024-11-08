eb4b4fcd4c7ca54f253ccbaf208f45b3aa31d0c2
elliotxx <951376975@qq.com>
2024-11-08 20:14:00 +0800

    fix: invalid oh-my-zsh config

--- modules/shell.nix
+++ modules/shell.nix
@@ -1,28 +1,29 @@
 { config, pkgs, ... }:
 
 let
+  # 获取 oh-my-zsh
+  oh-my-zsh = pkgs.fetchFromGitHub {
+    owner = "ohmyzsh";
+    repo = "ohmyzsh";
+    rev = "b4f9698733d7b29cc495e649e26fd6c3a5dcfcae";  # 使用最新的稳定版本
+    sha256 = "sha256-45qTga1yXg40pdQgqN/iBJmVMxvJ8Qk8c3UVwbxqd2o=";
+  };
+
   # 获取 dracula 主题
   dracula-theme = pkgs.fetchFromGitHub {
     owner = "dracula";
     repo = "zsh";
-    rev = "v1.2.5";  # 使用固定版本
+    rev = "v1.2.5";
     sha256 = "sha256-4lP4++Ewz00siVnMnjcfXhPnJndE6ANDjEWeswkmobg=";
   };
 in
 {
   # 系统级 zsh 配置
-  programs.zsh = {
-    enable = true;
-    ohMyZsh = {
-      enable = true;
-      package = pkgs.oh-my-zsh;
-    };
-  };
+  programs.zsh.enable = true;
 
   # 确保必要的包被安装
   environment.systemPackages = with pkgs; [
     zsh
-    oh-my-zsh
     fzf
     autojump
     kubectx
@@ -33,13 +34,14 @@ in
     home = {
       stateVersion = "24.05";
       
-      # 安装 dracula 主题
+      # 安装 oh-my-zsh 和主题
       file = {
-        ".oh-my-zsh/custom/themes/dracula.zsh-theme" = {
-          source = "${dracula-theme}/dracula.zsh-theme";
+        ".oh-my-zsh" = {
+          source = oh-my-zsh;
+          recursive = true;
         };
-        ".oh-my-zsh/custom/lib/lib" = {
-          source = "${dracula-theme}/lib";
+        ".oh-my-zsh/custom/themes/dracula" = {
+          source = dracula-theme;
           recursive = true;
         };
       };
@@ -48,18 +50,30 @@ in
     programs.zsh = {
       enable = true;
       
-      # oh-my-zsh 配置
-      oh-my-zsh = {
-        enable = true;
-        theme = "dracula";
-        plugins = [
-          "git"
-          "autojump"
-          "history-substring-search"
-          "fzf"
-          "kubectl"
-        ];
-      };
+      # 初始化配置
+      initExtraFirst = ''
+        # oh-my-zsh 配置
+        export ZSH="$HOME/.oh-my-zsh"
+        ZSH_THEME="dracula/dracula"
+        
+        # 启用的插件
+        plugins=(
+          git
+          autojump
+          history-substring-search
+          fzf
+          kubectl
+        )
+
+        # 加载 oh-my-zsh
+        source $ZSH/oh-my-zsh.sh
+      '';
+
+      initExtra = ''
+        # fzf 配置
+        export FZF_BASE=${pkgs.fzf}/bin/fzf
+        export FZF_COMPLETION_TRIGGER='~~'
+      '';
 
       # 插件配置
       plugins = [
@@ -82,22 +96,6 @@ in
           };
         }
       ];
-
-      # 初始化配置
-      initExtraFirst = ''
-        # 基础环境变量
-        export SHELL=${pkgs.zsh}/bin/zsh
-        export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
-      '';
-
-      initExtra = ''
-        # 加载 oh-my-zsh
-        source $ZSH/oh-my-zsh.sh
-
-        # fzf 配置
-        export FZF_BASE=${pkgs.fzf}/bin/fzf
-        export FZF_COMPLETION_TRIGGER='~~'
-      '';
 
       # 别名配置
       shellAliases = {
