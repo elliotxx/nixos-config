4b67843bc24d88f32952419094782df77e845b73
elliotxx <951376975@qq.com>
2024-11-13 17:35:01 +0800

    refactor: users/elliotxx

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
     # Home Manager for user environment management
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -28,21 +32,13 @@
           # Main system configuration
           ./configuration.nix
           
-          # Hardware configuration
-          ./hardware-configuration.nix
-          
           # Home Manager configuration
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.elliotxx = import ./users/elliotxx/default.nix;
+            home-manager.users.elliotxx = import ./users/elliotxx/home.nix;
           }
-          
-          # # Other modules
-          # ./modules/packages.nix
-          # ./modules/shell.nix
-          # ./modules/home.nix
         ];
       };
     };
--- users/elliotxx/home.nix
+++ users/elliotxx/home.nix
@@ -1,29 +1,88 @@
 { config, pkgs, ... }:
 
-let
-  shared = import ../../modules/shared.nix { inherit config pkgs; };
-in
 {
-  # 用户级配置
-  home-manager.users.elliotxx = { pkgs, ... }: {
-    # 启用 home-manager 管理
-    programs.home-manager.enable = true;
+  home.username = "elliotxx"; 
+  home.homeDirectory = "/home/elliotxx";
 
-    # 基础配置
-    home = {
-      username = "elliotxx";
-      homeDirectory = "/home/elliotxx";
-      stateVersion = "24.05";
-    };
+  # 通过 home.packages 安装一些常用的软件
+  # 这些软件将仅在当前用户下可用，不会影响系统级别的配置
+  # 建议将所有 GUI 软件，以及与 OS 关系不大的 CLI 软件，都通过 home.packages 安装
+  home.packages = with pkgs;[
+    neofetch
+    nnn # terminal file manager
 
-    # git 配置
-    programs.git = {
-      enable = true;
-      userName = "elliotxx";
-      userEmail = "951376975@qq.com";
-    };
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
 
-    # 导入共享的 zsh 配置
-    imports = [ (shared.mkZshConfig) ];
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # misc
+    file
+    which
+    tree
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    btop  # replacement of htop/nmon
+
+    # system call monitoring
+    lsof # list open files
+  ];
+
+  # git 相关配置
+  programs.git = {
+    enable = true;
+    userName = "elliotxx";
+    userEmail = "951376975@qq.com";
   };
+
+  # 启用 starship，这是一个漂亮的 shell 提示符
+  programs.starship = {
+    enable = true;
+    # 自定义配置
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+
+    shellAliases = {
+      k = "kubectl";
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 } 
