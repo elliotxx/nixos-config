9bd71a84ed9db8951d8d3062345ab16dd399d347
elliotxx <951376975@qq.com>
2024-11-14 19:37:59 +0800

    refactor: restore shell.nix

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,7 @@
 {
   imports = [
     ./modules/boot.nix
+    ./modules/shell.nix
     ./modules/desktop.nix
     ./modules/networking.nix
     ./modules/packages.nix
---
+++ modules/shell.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+  # 全局 shell 配置
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
+} 
